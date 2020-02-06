25d7859ee598a138f3a914f421bb95794b07d7e4
ixon7 <55660649+ixon7@users.noreply.github.com>
2020-02-06 17:06:12 +0100

    Dodaj pliki projektów.

---
+++ Kolokwium2_06_02_2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kolokwium2_06_02_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
